e826e3957f65d315d39d5bcf5e752b94359ed79e
haybre <haybreapril@gmail.com>
2026-05-09 15:36:04 +0200

    added runner class

---
+++ src/test/java/Runner/runner.java
@@ -0,0 +1,15 @@
+package Runner;
+
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions (
+        features = "src/test/resources/features",
+        glue = {"Steps"},
+        tags = "@createGroup",
+        plugin = {"html:target/cucumber-report.html"},
+        monochrome = true,
+        publish = true
+)
+
+public class runner {
+}
