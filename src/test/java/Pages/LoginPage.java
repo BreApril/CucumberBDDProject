473b2aad2f704967c55a3ba91bd03cac9bd5ed47
haybre <haybreapril@gmail.com>
2026-05-16 13:34:55 +0200

    created HomePage and LoginPage(elements&methods)

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,4 +1,43 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class LoginPage {
+    WebDriver driver;
+
+    @FindBy(xpath = "//span[normalize-space()='Access Learning Materials']")
+    WebElement loginHeading_xpath;
+
+    @FindBy(id = "login-email")
+    WebElement loginEmail_id;
+
+    @FindBy(id = "login-password")
+    WebElement password_id;
+
+    @FindBy(id = "login-submit")
+    WebElement loginButton;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void verifyLoginPageIsDisplayed() {
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOf(loginHeading_xpath));
+        loginHeading_xpath.isDisplayed();
+    }
+    public void enterEmail(String email) {
+        loginEmail_id.sendKeys(email);
+    }
+    public void enterPassword(String password) {
+        password_id.sendKeys(password);
+    }
+    public void clickLoginButton() {
+        loginButton.click();
+    }
 }
